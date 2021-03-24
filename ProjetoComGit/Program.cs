6c26371158603b0f807e19093ca23a1b43ec95d1
Willes Santos Pinto <willes.barao@gmail.com>
2021-03-24 11:28:15 -0300

    Mudei o nome do olá mundo para versão BR

--- ProjetoComGit/Program.cs
+++ ProjetoComGit/Program.cs
@@ -6,7 +6,7 @@ namespace ProjetoComGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo");
         }
     }
 }
